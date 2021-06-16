16e93fcd404ec9ee63bf39694d42162c0d15918c
S-I <sandra.ibrahim@hotmail.fr>
2021-06-16 20:39:55 +0200

    [ADD] previewHomePage component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    <PreviewHomePage />
+    <v-container fluid class="grey lighten-5">
+      <v-row v-for="n in 3" :key="n">
+        <v-col>
+          <PreviewHomePage :cardInformationArray="cards"/>
+        </v-col>
+      </v-row>
+    </v-container>
+    
+
   </div>
 </template>
 
@@ -11,6 +19,16 @@ export default {
   name: 'Home',
   components: {
     PreviewHomePage
+  },
+  data () {
+    return {
+      cards: [
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
+        { title: 'Best', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
+        { title: 'Best air', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' }
+      ],
+    }
   }
 }
 </script>

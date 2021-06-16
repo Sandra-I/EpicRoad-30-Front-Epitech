08f7c6b7ff465507c763055e070ff971394664b9
S-I <sandra.ibrahim@hotmail.fr>
2021-06-16 19:33:36 +0200

    [ADD] create component home page

---
+++ src/components/PreviewHomePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="preview-homepage">
+    <h1>{{ titlePreviewBloc }}</h1>
+    <div id="app">
+      <v-row>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PreviewHomePage',
+  props: {
+    titlePreviewBloc: {
+      type: String,
+      default: 'Le titre'
+    }
+  },
+  data () {
+    return {
+      cards: [
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 }
+      ],
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PreviewHomePage />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PreviewHomePage from '@/components/PreviewHomePage.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    PreviewHomePage
   }
 }
 </script>
